717860a9d8f085b4b7e56ab16589504fb80274bc
Willie <116541069+yuuytoi@users.noreply.github.com>
2022-12-24 16:57:10 +0800

    Add files via upload

---
+++ windows/QMK Toolbox/Helpers/EmbeddedResourceHelper.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace QMK_Toolbox.Helpers
+{
+    public static class EmbeddedResourceHelper
+    {
+        public static readonly string[] Resources =
+        {
+            "avrdude.conf",
+            "reset.eep",
+            "reset_left.eep",
+            "reset_right.eep",
+            "avrdude.exe",
+            "bootloadHID.exe",
+            "dfu-programmer.exe",
+            "dfu-util.exe",
+            "hid_bootloader_cli.exe",
+            "mdloader.exe",
+            "teensy_loader_cli.exe",
+            "wb32-dfu-updater_cli.exe",
+            "libftdi1.dll",
+            "libusb0.dll",
+            "libusb-0-1-4.dll",
+            "libusb-1.0.dll",
+            "libwinpthread-1.dll"
+        };
+
+        public static void ExtractResource(string file)
+        {
+            var destPath = Path.Combine(Application.LocalUserAppDataPath, file);
+
+            if (!File.Exists(destPath))
+            {
+                using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"QMK_Toolbox.Resources.{file}"))
+                {
+                    using (var filestream = new FileStream(destPath, FileMode.Create))
+                    {
+                        stream?.CopyTo(filestream);
+                    }
+                }
+            }
+        }
+
+        public static void ExtractResources(params string[] files) => ExtractResources(files as IEnumerable<string>);
+        public static void ExtractResources(IEnumerable<string> files)
+        {
+            foreach (var s in files)
+            {
+                ExtractResource(s);
+            }
+        }
+
+        public static string GetResourceContent(string file)
+        {
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream($"QMK_Toolbox.Resources.{file}"))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
---
+++ windows/QMK Toolbox/HidConsole/HidConsoleDevice.cs
@@ -0,0 +1,94 @@
+﻿using HidLibrary;
+using System.Linq;
+using System.Text;
+
+namespace QMK_Toolbox.HidConsole
+{
+    public class HidConsoleDevice
+    {
+        public delegate void HidConsoleReportReceivedDelegate(HidConsoleDevice device, string data);
+
+        public HidConsoleReportReceivedDelegate consoleReportReceived;
+
+        public IHidDevice HidDevice { get; }
+
+        public string ManufacturerString { get; }
+
+        public string ProductString { get; }
+
+        public ushort VendorId { get; }
+
+        public ushort ProductId { get; }
+
+        public ushort RevisionBcd { get; }
+
+        public HidConsoleDevice(IHidDevice device)
+        {
+            HidDevice = device;
+            HidDevice.OpenDevice();
+
+            ManufacturerString = GetManufacturerString(HidDevice);
+            ProductString = GetProductString(HidDevice);
+            VendorId = (ushort)HidDevice.Attributes.VendorId;
+            ProductId = (ushort)HidDevice.Attributes.ProductId;
+            RevisionBcd = (ushort)HidDevice.Attributes.Version;
+
+            HidDevice.MonitorDeviceEvents = true;
+            HidDevice.ReadReport(HidDeviceReportEvent);
+
+            HidDevice.CloseDevice();
+        }
+
+        public override string ToString()
+        {
+            return $"{ManufacturerString} {ProductString} ({VendorId:X4}:{ProductId:X4}:{RevisionBcd:X4})";
+        }
+
+        private string currentLine = "";
+
+        private void HidDeviceReportEvent(HidReport report)
+        {
+            if (HidDevice.IsConnected)
+            {
+                // Check if we have a completed line queued
+                int lineEnd = currentLine.IndexOf('\n');
+                if (lineEnd == -1)
+                {
+                    // Partial line or nothing - append incoming report to current line
+                    string reportString = Encoding.UTF8.GetString(report.Data).Trim('\0');
+                    currentLine += reportString;
+                }
+
+                // Check again for a completed line
+                lineEnd = currentLine.IndexOf('\n');
+                while (lineEnd >= 0)
+                {
+                    // Fire delegate with completed lines until we have none left
+                    string completedLine = currentLine.Substring(0, lineEnd);
+                    currentLine = currentLine.Substring(lineEnd + 1);
+                    lineEnd = currentLine.IndexOf('\n');
+                    consoleReportReceived?.Invoke(this, completedLine);
+                }
+
+                // Reregister this callback
+                HidDevice.ReadReport(HidDeviceReportEvent);
+            }
+        }
+
+        private static string GetManufacturerString(IHidDevice d)
+        {
+            if (d == null) return "";
+
+            d.ReadManufacturer(out var bs);
+            return System.Text.Encoding.Default.GetString(bs.Where(b => b > 0).ToArray());
+        }
+
+        private static string GetProductString(IHidDevice d)
+        {
+            if (d == null) return "";
+
+            d.ReadProduct(out var bs);
+            return System.Text.Encoding.Default.GetString(bs.Where(b => b > 0).ToArray());
+        }
+    }
+}
